934fb11b228cd92e9de8e14473c5d83ac0271a18
Halfwhit <Halfwhit@gmail.com>
2020-06-04 16:53:16 +0100

    Return 404 page if not a GET request to index.html

--- src/main.rs
+++ src/main.rs
@@ -15,18 +15,18 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
-
     if buffer.starts_with(get) {
         let contents = fs::read_to_string("web/index.html").unwrap();
-
         let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     } else {
-        // Not a GET request
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+        let contents = fs::read_to_string("web/404.html").unwrap();
+        let response = format!("{}{}", status_line, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
     }
 }
